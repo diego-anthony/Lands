88299e69cdb6a94298e872031260cc5a2a476d4f
Diego <dsanthony997@gmail.com>
2018-02-21 16:45:58 -0500

    Structure and Login

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
